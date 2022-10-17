ac77e570ac404a6de948dc1dc7a0a1fac506b281
GrigoriyKostryukov <kostryukov-grigoriy@mail.ru>
2022-10-17 19:31:38 +0300

    Add function to convert Roman numbers to Arabic notation

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,41 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    for (let i = 0; i < str.length; ++i) {
+        switch (str[i]) {
+            case 'M':
+                result += 1000;
+                break;
+            case 'D':
+                result += 500;
+                break;
+            case 'C':
+                result +=
+                    i < str.length - 1 &&
+                    (str[i + 1] == 'M' || str[i + 1] == 'D')
+                        ? -100
+                        : 100;
+                break;
+            case 'L':
+                result += 50;
+                break;
+            case 'X':
+                result +=
+                    i < str.length - 1 &&
+                    (str[i + 1] == 'C' || str[i + 1] == 'L')
+                        ? -10
+                        : 10;
+                break;
+            case 'V':
+                result += 5;
+                break;
+            case 'I':
+                result +=
+                    i < str.length - 1 &&
+                    (str[i + 1] == 'X' || str[i + 1] == 'V')
+                        ? -1
+                        : 1;
+                break;
+        }
+    }
     return result;
 }
